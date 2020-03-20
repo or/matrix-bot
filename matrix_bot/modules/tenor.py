76626f6b280313af3bef87501c05c2fec1608664
Oliver Runge <oliver.runge@gmail.com>
2020-03-20 12:24:42 +0100

    Handle case of no tenor results

--- matrix_bot/modules/tenor.py
+++ matrix_bot/modules/tenor.py
@@ -31,8 +31,24 @@ class TenorModule(MatrixBotModule):
         response = requests.get("https://api.tenor.com/v1/search",
                                 params=dict(api_key=self.config['tenor']['api_key'],
                                             limit=20,
+                                            media_filter='minimal',
                                             q=search))
         results = json.loads(response.content.decode('utf-8'))
+
+        title = 'Sir'
+        if ('users' in self.config
+                and 'female' in self.config['users']
+                and event['sender'] in self.config['users']['female'].split()):
+            title = 'Miss'
+
+        if 'results' not in results:
+            room_.send_text(f"It appears something went wrong, {title}.")
+            return
+
+        if not results['results']:
+            room_.send_text(f"That doesn't exist, {title}.")
+            return
+
         match = random.choice(results['results'])
         title = match['title']
         url = match['media'][0]['gif']['url']
